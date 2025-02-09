bcadfa5bca0eefdb40b3423e9fc668c987311b58
CodePicsel <bankarshashank9@gmail.commkdir>
2025-02-09 17:41:17 +0530

    Update add task message

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ function listTasks() {
   tasks.forEach((task, index) => console.log(`${index + 1}. ${task}`));
 }
 
+function deleteTask(index) {
+  if (index >= 0 && index < tasks.length) {
+    const removed = tasks.splice(index, 1);
+    console.log(`Deleted: ${removed}`);
+  }
+}
+
 // Test
 addTask("Learn Git");
 addTask("Build Todo List");
